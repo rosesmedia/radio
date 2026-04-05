aabb7870151092f56dbc8b370d6880161f70f43e
Luis <luis@lu15.dev>
2026-04-05 23:35:27 +0100

    feat!: add api route for switching stream source

--- stream-controller/src/liquidsoap.rs
+++ stream-controller/src/liquidsoap.rs
@@ -1,11 +1,15 @@
 use std::path::PathBuf;
 
+use miette::{Context, IntoDiagnostic};
 use tokio::{io::AsyncWriteExt as _, net::UnixStream};
 
 #[derive(Debug, Clone)]
 pub struct LiquidsoapClient {
     path: PathBuf,
 }
+
+#[derive(Debug)]
+pub struct LiquidsoapConnection(UnixStream);
 
 #[derive(Debug, Clone, Copy)]
 pub enum Source {
@@ -16,36 +20,46 @@ pub enum Source {
 }
 
 impl LiquidsoapClient {
+    pub fn new(stream: &str) -> Self {
+        todo!("this allows for path traversal");
+        Self {
+            path: format!("/tmp/restreamer-control-{stream}.sock").into(),
+        }
+    }
+
+    pub async fn create_connection(&self) -> miette::Result<LiquidsoapConnection> {
+        Ok(LiquidsoapConnection(
+            UnixStream::connect(&self.path)
+                .await
+                .into_diagnostic()
+                .with_context(|| "failed to connect to unix socket")?,
+        ))
+    }
+}
+
+impl LiquidsoapConnection {
     const ACTION_SEND_COMMAND: &'static [u8] = b"\n";
 
-    fn new<P: Into<PathBuf>>(path: P) -> Self {
-        Self { path: path.into() }
-    }
+    async fn send_command(&mut self, command: &[u8]) -> Result<(), std::io::Error> {
+        let Self(stream) = self;
 
-    async fn create_connection(&self) -> Result<UnixStream, std::io::Error> {
-        UnixStream::connect(&self.path).await
-    }
-
-    async fn send_command(
-        &self,
-        connection: &mut UnixStream,
-        command: &[u8],
-    ) -> Result<(), std::io::Error> {
         // make sure connection is writable
-        connection.writable().await?;
+        stream.writable().await?;
 
         // send command
-        connection.write(command).await?;
-        connection.write(Self::ACTION_SEND_COMMAND).await?;
+        stream.write(command).await?;
+        stream.write(Self::ACTION_SEND_COMMAND).await?;
 
         // flush connection
-        connection.flush().await
+        stream.flush().await
     }
 
-    pub async fn set_source(&self, source: &Source) -> Result<(), std::io::Error> {
+    pub async fn set_source(&mut self, source: &Source) -> miette::Result<()> {
         let command = format!("var.set source={}", source.id());
-        let mut connection = self.create_connection().await?;
-        self.send_command(&mut connection, command.as_bytes()).await
+        self.send_command(command.as_bytes())
+            .await
+            .into_diagnostic()
+            .with_context(|| "failed to write to unix socket")
     }
 }
 
@@ -58,8 +72,14 @@ impl Source {
             Source::TechnicalDifficulties => 4,
         }
     }
-}
 
-pub fn get_client(id: &str) -> LiquidsoapClient {
-    LiquidsoapClient::new(format!("/tmp/restreamer-control-{id}.sock"))
+    pub fn from_id(id: u8) -> Option<Self> {
+        match id {
+            1 => Some(Source::Live),
+            2 => Some(Source::PreStream),
+            3 => Some(Source::PostStream),
+            4 => Some(Source::TechnicalDifficulties),
+            _ => None,
+        }
+    }
 }
--- stream-controller/src/routes.rs
+++ stream-controller/src/routes.rs
@@ -1,57 +1,95 @@
 use axum::{
-    Json, Router,
+    Router,
     extract::{Path, State},
     http::StatusCode,
-    response::IntoResponse,
+    response::{IntoResponse, Response},
     routing::post,
 };
-use serde::Serialize;
 
-use crate::{AppState, auth};
+use crate::{
+    AppState, auth,
+    liquidsoap::{LiquidsoapClient, Source},
+};
 
-#[derive(Serialize)]
-#[serde(untagged)]
-pub enum ApiResult {
-    Success { ok: bool },
-    Error { message: String },
+enum ApiError {
+    NotFound,
+    Internal,
 }
 
-impl From<miette::Result<()>> for ApiResult {
-    fn from(value: miette::Result<()>) -> Self {
-        match value {
-            Ok(()) => ApiResult::Success { ok: true },
-            Err(e) => {
-                tracing::error!(?e, "failed to start stream");
-                ApiResult::Error {
-                    message: format!("{e}"),
-                }
-            }
-        }
-    }
-}
-
-impl IntoResponse for ApiResult {
-    fn into_response(self) -> axum::response::Response {
+impl ApiError {
+    fn status_code(&self) -> StatusCode {
         match self {
-            Self::Success { .. } => Json(self).into_response(),
-            Self::Error { .. } => (StatusCode::INTERNAL_SERVER_ERROR, Json(self)).into_response(),
+            Self::NotFound => StatusCode::NOT_FOUND,
+            Self::Internal => StatusCode::INTERNAL_SERVER_ERROR,
         }
+    }
+
+    fn message(&self) -> String {
+        match self {
+            Self::NotFound => "not found",
+            Self::Internal => "internal server error",
+        }
+        .to_string()
+    }
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        (self.status_code(), self.message()).into_response()
+    }
+}
+
+impl<E> From<E> for ApiError
+where
+    E: Into<miette::Report>,
+{
+    fn from(err: E) -> Self {
+        tracing::error!("{:?}", err.into());
+        ApiError::Internal
     }
 }
 
 #[tracing::instrument(skip(state))]
-async fn start_streamer(State(state): State<AppState>, Path(id): Path<String>) -> ApiResult {
-    state.service_manager.start_streamer(&id).await.into()
+async fn start_streamer(
+    State(state): State<AppState>,
+    Path(id): Path<String>,
+) -> Result<StatusCode, ApiError> {
+    state.service_manager.start_streamer(&id).await?;
+    Ok(StatusCode::NO_CONTENT)
 }
 
 #[tracing::instrument(skip(state))]
-async fn stop_streamer(State(state): State<AppState>, Path(id): Path<String>) -> ApiResult {
-    state.service_manager.stop_streamer(&id).await.into()
+async fn stop_streamer(
+    State(state): State<AppState>,
+    Path(id): Path<String>,
+) -> Result<StatusCode, ApiError> {
+    state.service_manager.stop_streamer(&id).await?;
+    Ok(StatusCode::NO_CONTENT)
+}
+
+#[tracing::instrument]
+async fn switch_stream_source(
+    Path((id, source)): Path<(String, u8)>,
+) -> Result<StatusCode, ApiError> {
+    let source = Source::from_id(source);
+    let Some(source) = source else {
+        return Err(ApiError::NotFound);
+    };
+
+    let client = LiquidsoapClient::new(&id);
+    let mut connection = client
+        .create_connection()
+        .await
+        .map_err(|_| ApiError::NotFound)?;
+    connection.set_source(&source).await?;
+
+    Ok(StatusCode::NO_CONTENT)
 }
 
 pub fn routes() -> Router<AppState> {
     Router::new()
         .route("/stream/{id}/start", post(start_streamer))
         .route("/stream/{id}/stop", post(stop_streamer))
+        .route("/stream/{id}/switch/{source}", post(switch_stream_source))
         .layer(axum::middleware::from_fn(auth::auth_middleware))
 }
