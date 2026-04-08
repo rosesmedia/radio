82b03813e4cef949cc793065247baff835e02383
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-04-08 21:48:02 +0100

    chore: add CI for stream-controller

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, crane, flake-utils }:
     flake-utils.lib.eachDefaultSystem(system:
       let
         pkgs = import nixpkgs {
@@ -16,14 +17,21 @@
         devDependencies = ps: with ps; [
           python-lsp-server
         ] ++ (dependencies ps);
+
+        streamControllerDependencies = with pkgs; [
+        ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.libiconv
+        ];
+
+        craneLib = crane.mkLib pkgs;
       in
       {
-        devShells.default = pkgs.mkShell {
+        devShells.default = craneLib.devShell {
           LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath(with pkgs; [
             libpq
             file
-          ])}";
-          nativeBuildInputs = with pkgs; [
+          ] ++ streamControllerDependencies)}";
+          packages = with pkgs; [
             libpq
             file
             (python313.withPackages devDependencies)
@@ -31,7 +39,8 @@
             ffmpeg
             liquidsoap
             socat
-          ];
+            rust-analyzer
+          ] ++ streamControllerDependencies;
         };
       }
     );
