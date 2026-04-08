f7ac0e183bd8f40f4927ec4b84b3d774946f7f76
Ashhhleyyy <ash@ashhhleyyy.dev>
2026-04-08 20:37:09 +0100

    feat: add htmx and site-wide hx-boost

--- roses_radio/settings.py
+++ roses_radio/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     'social_django',
     'django_bootstrap5',
+    'django_htmx',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'roses_radio.urls'
